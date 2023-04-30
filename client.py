37bb2d3e8fb94605e645af0f97516bcdd69484a5
OmaRodriguez-13 <al056964@uacam.mx>
2023-04-29 21:47:15 -0600

    Initial commit

---
+++ client.py
@@ -0,0 +1,90 @@
+import Pyro4
+import tkinter as tk
+from tkinter import messagebox, simpledialog, scrolledtext
+from tkinter import *
+
+class Interfaz:
+    def __init__(self, uri):
+        self.juego = Pyro4.Proxy(uri)
+
+        self.root = tk.Tk()
+        self.root.title("SECRET WORD!")
+        # self.root.iconbitmap('urna.ico')
+        self.root.geometry('390x300')
+
+        # Hacer que la ventana no sea redimensionable
+        self.root.resizable(False, False)
+
+        self.P = tk.Label(self.root, text="Pista principal:",
+                          font=("Courier New", 12))
+        self.P.pack(pady=10)
+
+        self.scrollbar = tk.Scrollbar(self.root, width=13)
+        self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        self.text_area = scrolledtext.ScrolledText(self.root, wrap=tk.WORD, yscrollcommand=self.scrollbar.set, height=8, width=35)
+        self.text_area.pack(pady=12)
+
+        # Configurar el Scrollbar para que funcione con el ScrolledText
+        self.scrollbar.config(command=self.text_area.yview)
+
+         # Obtener opciones y tema del servidor
+        self.temav = self.juego.obtener_prin()
+        self.word = self.juego.obtener_palabra()
+        self.pista1 = self.juego.obtener_pistas()
+
+        # Establecer el contenido del ScrolledText
+        self.text_area.insert(tk.INSERT, self.temav)
+
+        self.idk = tk.Label(self.root, text="¡Trata de adivinar la secret word!")
+        self.idk.pack(pady=10)
+
+        self.entry = Entry(self.root, width=30)
+        self.entry.pack()
+        self.button = Button(self.root, text="Adivinar", command=self.adivinar)
+        self.button.pack(pady=10)
+
+        self.root.update_idletasks()
+        width = self.root.winfo_width()
+        height = self.root.winfo_height()
+        x = (self.root.winfo_screenwidth() // 2) - (width // 2)
+        y = (self.root.winfo_screenheight() // 2) - (height // 2)
+        self.root.geometry('{}x{}+{}+{}'.format(width, height, x, y))
+
+
+    def adivinar(self):
+        palabra = self.entry.get()
+        resultado = self.juego.adivinar(palabra)
+        messagebox.showinfo("Resultado", resultado)
+
+
+    def iniciar(self):
+        self.root.mainloop()
+
+
+user = simpledialog.askstring(
+    "Username", "Ingrese su nombre de usuario:")
+if user is None:
+    # Si el usuario cancela el diálogo, salir del programa
+    exit()
+
+server_ip = simpledialog.askstring(
+    "Dirección IP del servidor", "Ingrese la dirección IP del servidor:")
+if server_ip is None:
+    # Si el usuario cancela el diálogo, salir del programa
+    exit()
+
+server_port = simpledialog.askstring(
+    "Dirección IP del servidor", "Ingrese el puerto del servidor:")
+if server_port is None:
+    # Si el usuario cancela el diálogo, salir del programa
+    exit()
+uri = f"PYRO:juego@{server_ip}:{server_port}"
+
+messagebox.showinfo("URI del SERVIDOR", uri)
+print("URI del SERVIDOR", uri)
+
+messagebox.showinfo("Regla", "Introduce sólo minúsculas", icon="warning")
+
+interfaz = Interfaz(uri)
+interfaz.iniciar()
+
---
+++ server.py
@@ -0,0 +1,75 @@
+import Pyro4
+import tkinter as tk
+from tkinter import messagebox, simpledialog
+import random
+import sys
+
+@Pyro4.expose
+class JuegoAdivinarPalabra:
+    def __init__(self):
+        self.palabra = ""
+        self.pista1 = ""
+        self.pistaPrin = ""
+        self.pista2 = ""
+        self.intentos_fallidos = 0
+
+    def establecer_palabra(self, palabra):
+        self.palabra = palabra.lower()
+
+    def obtener_palabra(self):
+        return self.palabra
+
+    def establecer_pistas(self, pista1):
+        self.pista1 = pista1
+        self.pista2 = "La palabra empieza con la letra: " + str(self.palabra[0]) + " y tiene " + str(len(self.palabra)) + " letras."
+
+    def obtener_pistas(self):
+        if self.intentos_fallidos == 0:
+            #return "Adivina la palabra."
+            return self.pistaPrin
+        elif self.intentos_fallidos == 1:
+            return "¡Fallaste! Aquí tienes una pista:\n" + self.pista1
+        elif self.intentos_fallidos == 2:
+            return "¡Fallaste de nuevo! Aquí tienes otra pista:\n" + self.pista2
+        else:
+            return "Lo siento, has perdido. La palabra era " + self.palabra + "."
+            
+
+    def adivinar(self, palabra):
+        if palabra.lower() == self.palabra:
+            return "¡Felicidades, has ganado!"
+        else:
+            self.intentos_fallidos += 1
+            if self.intentos_fallidos <= 2:
+                return "No es la palabra. " + self.obtener_pistas()
+            else:
+                return self.obtener_pistas()
+            
+    def establecer_prin(cls, pistaPrin):
+        cls.pistaPrin = pistaPrin
+
+    def obtener_prin(cls):
+        return cls.pistaPrin
+
+
+ip = Pyro4.socketutil.getInterfaceAddress("192.168.137.1")
+daemon = Pyro4.Daemon(host=ip)
+
+juego = JuegoAdivinarPalabra()
+uri = daemon.register(juego, objectId='juego')
+
+# Obtener la palabra del usuario
+palabra = simpledialog.askstring("Adivinar la palabra", "Ingresa la palabra a adivinar:")
+pistaPrin = simpledialog.askstring("Pista principal", "Ingresa la pista principal:")
+pista1 =  simpledialog.askstring("Pista", "Ingresa una nueva pista:")
+
+# Establecer la palabra y pista en el servidor
+juego.establecer_palabra(palabra)
+juego.establecer_prin(pistaPrin)
+juego.establecer_pistas(pista1)
+
+messagebox.showinfo("URI del SERVIDOR", f"Servidor listo. URI= {uri}")
+print("Servidor listo. URI= ", uri)
+
+daemon.requestLoop()
+
